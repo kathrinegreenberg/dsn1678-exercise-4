be9e36da9c33193d512d8e622fdb18371c5e7f46
KathrineGreenberg <kathrinegreenberg@gmail.com>
2014-10-07 08:54:06 -0400

    Edited the background.
    Added sun section variable in js. Edited the background position when
    scrolling in js.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 var $win = $(window);
 /*dollar sign is a reminder that it is a jquery object*/
 var $sun = $('.sun');
+var $sunSection = $('.sun-section');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
-	
+	 
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+	$sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
 });
 /*console.log(); is basically doing a test to see if what your doing is working by displaying whatever is in the brackets in the console in inspect element.
 	(' + scrollPos + 'deg) is connecting the rotation to the scrolling. the sun rotates in the same increments as you scroll up/down the page.*/
