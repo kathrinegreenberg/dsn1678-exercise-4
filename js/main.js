0c33c8ecc3fadbb31eda678d89530bd40eadfadd
KathrineGreenberg <kathrinegreenberg@gmail.com>
2014-10-07 10:29:02 -0400

    Added shuttle variables and
    Made shuttle and shuttle class variables in js. Added waypoint function
    for shuttle section to js. In css, added transform to keyframe with
    opacity.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipper = $('.dipper');
 var $dipperSection = $('.dipper-section');
+var $shuttle = $('.shuttle');
+var $shuttleSection = $('.shuttle-section');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -18,3 +20,7 @@ $dipperSection.waypoint(function() {
 	$dipper.addClass('js-dipper-fade');
 }, { offset: '50%' });
 /*add offset if you want it to be different than the default*/
+
+$shuttleSection.waypoint(function() {
+	$shuttle.addClass('js-shuttle-fade');
+}, { offset: '50%' });
