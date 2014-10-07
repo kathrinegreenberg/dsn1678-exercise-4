0af70c522079f4f357236b8819f424044b5b4d05
KathrineGreenberg <kathrinegreenberg@gmail.com>
2014-10-07 09:18:12 -0400

    Added dipper image.
    Added section including the dipper image in html, both with classes.
    Added a width to the image in css.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 	 
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
-	$sunSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
+	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 });
 /*console.log(); is basically doing a test to see if what your doing is working by displaying whatever is in the brackets in the console in inspect element.
 	(' + scrollPos + 'deg) is connecting the rotation to the scrolling. the sun rotates in the same increments as you scroll up/down the page.*/
